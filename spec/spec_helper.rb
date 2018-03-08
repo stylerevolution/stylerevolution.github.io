ea8dfa835515c0c5e639cf4a26afbf388a028e72
Marii <mn2802@columbia.edu>
2018-03-08 15:37:24 -0500

    regen tiles

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,18 +7,11 @@ require 'rack/test'
 RSpec.configure do |config|
   config.include Capybara::DSL
 
-  Capybara.register_driver :chrome do |app|
-    Capybara::Selenium::Driver.new(app, browser: :chrome)
-  end
-
   Capybara.register_driver :headless_chrome do |app|
     capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: { args: %w(headless disable-gpu) }
+      chromeOptions: { 'args' => %w{headless disable-popup-blocking no-sandbox disable-gpu window-size=1400,900} }
     )
-
-    Capybara::Selenium::Driver.new app,
-      browser: :chrome,
-      desired_capabilities: capabilities
+    Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
   end
 
   Capybara.javascript_driver = :headless_chrome
